f832baa3e71cbab93ba1aab01d9bf4594044b685
priya 123 <priyakatare1707@gmail.com>
2026-01-13 19:35:22 +0530

    new

--- raj.js
+++ raj.js
@@ -1,10 +1,33 @@
-document.getElementById('btn1').addEventListener('click',mysum);
-function mysum(){
+// document.getElementById('btn1').addEventListener('click',mysum);
+// function mysum(){
 
 
-    let no1 =Number(document.getElementById('no1').value);
-    let no2 =Number(document.getElementById('no2').value);
-    let ans= no1+no2;
-    document.getElementById('demo').innerHTML ='Addition:'+ans;
- }
- 
+//     let no1 =Number(document.getElementById('no1').value);
+//     let no2 =Number(document.getElementById('no2').value);
+//     let ans= no1+no2;
+//     document.getElementById('demo').innerHTML ='Addition:'+ans;
+//  }
+//  
+//  document.getElementById('btn1').addEventListener('click',mycal);
+//  function mycal(){
+    
+//         let phy =Number(document.getElementById('phy').value);
+//          let che =Number(document.getElementById('che').value);
+//         let math =Number(document.getElementById('math').value);
+//         let eng =Number(document.getElementById('eng').value);
+
+//         let total =phy+che+math+eng;
+//         let per=total/4;
+//         document.getElementById('demo').innerHTML='addition:'  +  total;
+//         document.getElementById('demo2').innerHTML='percentage:' +  per;
+
+//  }
+// document.getElementById('btn1').addEventListener('click',mycal);
+//  function mycal(){
+//       let age =Number(document.getElementById('age').value);
+//       let height =Number(document.getElementById('height').value);
+//       let weight =Number(document.getElementById('weight').value);
+//       let  men=(10*weight)+(6.25*height)-(5*age)
+//       let women=(10*weight)+(60.25*height)-(5*age)-161
+
+//  }
